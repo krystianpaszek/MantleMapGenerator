d8c20a462791080b3f4c431909ba85902f5c8199
Krystian Paszek <krystianpaszek93@gmail.com>
2016-04-14 21:44:11 +0200

    Initial Commit

---
+++ Mantle Map Generator/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Mantle Map Generator
+//
+//  Created by Krystian Paszek on 14.04.2016.
+//  Copyright © 2016 Krystian Paszek. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
